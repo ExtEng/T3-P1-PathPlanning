c048b9a350da6196a201e824e64d14e00da612c0
Clifton Pereira <pereiraclifton@gmail.com>
2018-01-31 15:52:36 -0500

    Fixd typo with car speed

--- src/main.cpp
+++ src/main.cpp
@@ -256,15 +256,15 @@ int main() {
 			for (int i = 0; i < sensor_fusion.size(); i++)
 			{
 				float d = sensor_fusion[i][6];
-				if (d < (2+4*lane+2) && d > (2+4*lane-2))
+				if ((d < (2+4*lane+2) )&& (d > (2+4*lane-2)))
 				{
 					double vx = sensor_fusion[i][3];
 					double vy = sensor_fusion[i][4];
 					double check_speed = sqrt(vx*vx+vy*vy);
 					double check_car_s = sensor_fusion[i][5];
 					
-					check_car_s += ((double)prev_size*0.2*check_speed);
-					if ((check_car_s > car_s)&&((check_car_s - car_s) <30)){
+					check_car_s += ((double)prev_size*0.02*check_speed);
+					if ((check_car_s > car_s)&&((check_car_s - car_s) < 30)){
 						//ref_vel = 29.5;
 						too_close = true;
 						
